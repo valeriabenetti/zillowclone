e93a140cdbfe6d3d9211d89532dd439f23fd77bd
Valeria Benetti <vbenetti46@gmail.com>
2017-04-14 19:19:06 -0400

    Link to Index Page fixed
    Index page is looking somewhat static and I have two different displays. Currently working making each listing into a media object that are in colums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414164215) do
+ActiveRecord::Schema.define(version: 20170414211445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170414164215) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image_url"
   end
 
 end
