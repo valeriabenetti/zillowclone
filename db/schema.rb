825389a70ce519f14bcfa8a39e7074972a0368ae
Valeria Benetti <vbenetti46@gmail.com>
2017-04-15 14:23:37 -0400

    SignUp page added and given some style
    The sign up page has a form that allows the user to choose either the omniauth or the classic version. Still missing the actual ability to be able to sign in as well as a link to the page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414211445) do
+ActiveRecord::Schema.define(version: 20170415030848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,17 @@ ActiveRecord::Schema.define(version: 20170414211445) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image_url"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.integer  "phone"
+    t.string   "address"
+    t.string   "image_url"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
