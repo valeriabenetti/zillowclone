272b4a65364ddd719877b5c29a65baad3bf969e2
Valeria Benetti <vbenetti46@gmail.com>
2017-04-28 09:12:03 -0400

    Image Tags working even if there is no images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 
 //= require 'cable'
+//= require 'homes'
--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  $('.media-heading').on('click', function(event) {
+    $(this).css({color: 'teal'})
+  })
+})
