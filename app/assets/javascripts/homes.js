9d0a5003ae0d9f82e233035a1db655efc4c219c4
Valeria Benetti <vbenetti46@gmail.com>
2017-04-29 20:39:34 -0400

    Adding the favortie feature onto the template
    Trying to figure out where to place heart shape and as well image sizing onto the index of the home pages

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,9 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
-   $('.media-heading').on('click', function(event) {
-     $(this).css({color: 'orange'})
-   })
+
+  $('.clickable-heart').on('click', function(event) {
+    $(this).css({color: 'red'})
+  })
+  $('.media-heading').on('click', function(event) {
+    $(this).css({color: 'orange'})
+  })
 
   $('#query').on('input', function(event) {
     let queryValue = $(this).val()
