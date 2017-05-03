32d37e399bf4ddccb5f40fc654edb0843382e41c
Valeria Benetti <vbenetti46@gmail.com>
2017-05-03 16:02:59 -0400

    Modals are up and running
    Modals need some more work so that way the images viewed are the correct size instead of it being just the same image size as the original

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -9,11 +9,11 @@ class ImageUploader < Shrine
   process(:store) do |io, context|
     original = io.download
 
-    size_700 = resize_to_limit!(original, 700, 700)
-    size_450 = resize_to_limit!(size_700, 450, 450)
-    size_200 = resize_to_limit!(size_450, 150, 150)
+    size_800 = resize_to_limit!(original, 1000, 1000)
+    size_450 = resize_to_limit(size_800, 450, 450)
+    size_300 = resize_to_limit(size_450, 255, 250)
 
-    { original: io, large: size_700, medium: size_450, small: size_200 }
+    { original: io, large: size_800, medium: size_450, small: size_300 }
 
   end
 end
