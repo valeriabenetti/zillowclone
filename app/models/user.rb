8123c546e120621547149da7c96275d967527953
Valeria Benetti <vbenetti46@gmail.com>
2017-05-23 14:55:42 -0400

    Project is needing the mailers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
  class User < ApplicationRecord
+   attr_accessor :password
+   validates :email, presence: true, uniqueness: true
+   validates :password, confirmation: true # password_confirmation attr
+   validates_length_of :password, in: 6..20, on: :create
    has_secure_password
 
    has_many :homes
